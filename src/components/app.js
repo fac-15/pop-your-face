6731aa075e88e871a380beecca3f27a8fab54a50
Whooolia <tpdms2779@gmail.com>
2019-02-01 12:30:16 +0000

    Disappear each picture

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import Landing from './landing/landing'
-import {UserImage} from './githubapi/githubapi';
-import { githubData } from '../utlis/data_githubapi';
+import React from "react";
+import Landing from "./landing/landing";
+import { UserImage } from "./githubapi/githubapi";
+import { githubData } from "../utlis/data_githubapi";
 // import Obstacle from './obstacle';
 
-export default class App extends React.Component {  
-    state = {
-        userData: ''
-    }
+export default class App extends React.Component {
+  state = {
+    userData: "",
+    clicked: false,
+    images: Array.from({length:30}),
+    imagesClicked: []
+  };
 
-    handleUser = (value) => {
-        githubData(value).then(data => this.setState({userData: data}));
-    }
+  handleClick = (id) => {
+    this.setState({ clicked: true, imagesClicked: this.state.imagesClicked.concat(id) });
+  };
 
-    render() {
-        return(
-            <div>
-                <Landing handleUser={this.handleUser}/>
-                <UserImage userData={this.state.userData}/>
-                {/* <Obstacle /> */}
-            </div>
-        );
-    }
+  handleUser = value => {
+    githubData(value).then(data => this.setState({ userData: data }));
+  };
+
+  render() {
+    // let imageClassName = [''];
+    // let clickedClassName = 'clicked';
+    // console.log(this.state)
+    return (
+ 
+      <div>
+        <Landing handleUser={this.handleUser} />
+        {this.state.images.map((image, id) => (
+          <UserImage
+          key={id}
+          id={id}
+          userData={this.state.userData}
+          onClick={() => this.handleClick(id)}
+          className={this.state.imagesClicked.includes(id)?'clicked':id}
+          />))}
+      </div>
+    );
+  }
 }
-
--- src/components/githubapi/githubapi.js
+++ src/components/githubapi/githubapi.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./githubapi.css";
 
+export const UserImage = props => {
+  if (!props.userData) {
+    return <h3>Please wait :)</h3>;
+  }
 
-export const UserImage = (props) => {
-        if(!props.userData){
-            return <h3>Please wait :)</h3>
-        }
+  // const images = Array.from({ length: 30 });
+  // let imageClass = props.className;
+  // let clickedClass = props.className.push('clicked');
 
-        return (
-            <div>
-                <img src={props.userData.avatar_url}></img>
-            </div>
-        );
-}
+  return (
 
+    <img
+      src={props.userData.avatar_url}
+      onClick={props.onClick}
+      className={props.className}
+    />
+  );
+};
