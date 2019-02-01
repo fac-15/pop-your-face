816ec002e322472a6c68241b0c5142dc2abfaf43
Whooolia <tpdms2779@gmail.com>
2019-02-01 14:00:34 +0000

    styling

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Landing from "./landing/landing";
 import { UserImage } from "./githubapi/githubapi";
 import { githubData } from "../utlis/data_githubapi";
 import Counter from "./counter/counter";
+import './landing/landing.css';
 
 export default class App extends React.Component {
   state = {
@@ -28,7 +29,14 @@ export default class App extends React.Component {
       <div>
         <Landing handleUser={this.handleUser} />
         {!this.state.userData ? (
-          <h3>Please wait :)</h3>
+          <h3 class='loading'>💥How to play?💥
+          <p>Pop your face by clicking to get rid of Reactress(stress)!</p>
+            <ul>
+              <p>Type your github ID</p>
+              <p>Get rid of all your face in 30s!</p>
+              <p>Enjoy now :)</p>
+            </ul>
+          </h3>
         ) : (
           this.state.images.map((image, id) => (
             <UserImage
